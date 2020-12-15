567f7e43b6eb4534206bb0b9ff71346e63c808cb
pedroblanch <pedroblanch@iesjulioverne.es>
2020-12-15 09:16:17 +0100

    Añado la tabla del 3

--- index.php
+++ index.php
@@ -5,4 +5,9 @@ for($i=1;$i<=10;$i++){
 	$resultado=$i*2;
 	echo("<br>2 por $i = $resultado");
 }
+echo("<br><br>");
+for($i=1;$i<=10;$i++){
+	$resultado=$i*3;
+	echo("<br>3 por $i = $resultado");
+}
 ?>
